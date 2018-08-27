5e7cab6bb6cf5d33d4a143cd5ae35165ac7cb0b8
Chen Chang <chang.chen2@husky.neu.edu>
2018-08-27 11:07:22 -0400

    finish configuration and set up

---
+++ tests/simpleTest.py
@@ -0,0 +1,7 @@
+from payfacsdk import utils
+from payfacsdk import communication
+
+def test_get_retrieval_request(self):
+    suffix = 'get_retrieval_request'
+    config =  utils.Configuration()
+    # communication.http_get_retrieval_request(suffix,config)
