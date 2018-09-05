5be56abc1fb9c9d03d3a5a0636ceb67c343afb8d
Chen Chang <chang.chen2@husky.neu.edu>
2018-09-05 15:56:36 -0400

    add setup.py and generate.py

--- payfacsdk/utils.py
+++ payfacsdk/utils.py
@@ -1,7 +1,7 @@
 import os
 import json
 import pyxb
-# import xmltodict
+import xmltodict
 
 class Configuration(object):
     """Setup Configuration variables.
@@ -14,7 +14,7 @@ class Configuration(object):
         proxy (Str): Https proxy server address. Must start with "https://"
         print_xml (Str): Whether print request and response xml
     """
-
+    VERSION = u'13'
     _CONFIG_FILE_PATH = os.path.join(os.environ['PAYFAC_MP_SDK_CONFIG'], ".payfac_mp_sdk.conf") \
         if 'PAYFAC_MP_SDK_CONFIG' in os.environ else os.path.join(os.path.expanduser("~"), ".payfac_mp_sdk.conf")
 
@@ -65,92 +65,91 @@ class Configuration(object):
             json.dump(vars(self), config_file)
         return self._CONFIG_FILE_PATH
 
-# def obj_to_xml(obj):
-#     """Convert object to xml string without namespaces
-#
-#     Args:
-#         obj: Object
-#
-#     Returns:
-#         Xml string
-#
-#     Raises:
-#         pyxb.ValidationError
-#     """
-#     # TODO convert object to xml without default namespace gracefully.
-#     try:
-#
-#         xml = obj.toxml('utf-8')
-#     except pyxb.ValidationError as e:
-#         raise ChargebackError(e.details())
-#     xml = xml.replace(b'ns1:', b'')
-#     xml = xml.replace(b':ns1', b'')
-#     return xml
-#
-# def generate_retrieval_response(http_response, return_format='dict'):
-#     return convert_to_format(http_response.text, "chargebackRetrievalResponse", return_format)
-#
-#
-#
-#
-#
-# def convert_to_format(http_response, response_type, return_format='dict'):
-#     return_format = return_format.lower()
-#     if return_format == 'xml':
-#         response_xml = http_response.text
-#         return response_xml
-#     # elif return_format == 'object':
-#     #     return convert_to_obj(http_response.text)
-#     else:
-#         return convert_to_dict(http_response, response_type)
-#
-#
-# # def convert_to_obj(xml_response):
-# #     return fields_chargeback.CreateFromDocument(xml_response)
-#
-#
-# def convert_to_dict(xml_response, response_type):
-#     response_dict = xmltodict.parse(xml_response)[response_type]
-#     if response_dict['@xmlns'] != "":
-#         _create_lists(response_dict)
-#         return response_dict
-#     else:
-#         raise ChargebackError("Invalid Format")
-#
-#
-#
-#
-# def _create_lists(response_dict):
-#     if "chargebackCase" in response_dict:
-#         _create_list("chargebackCase", response_dict)
-#
-#         for case in response_dict["chargebackCase"]:
-#             if "activity" in case:
-#                 _create_list("activity", case)
-#
-#     if "errors" in response_dict:
-#         _create_list("error", response_dict["errors"])
-#
-#
-# # if there is only one element for the given key in container, create a list for it
-# def _create_list(element_key, container):
-#     element_value = container[element_key]
-#     if element_value != "" and not isinstance(element_value, list):
-#         container[element_key] = [element_value]
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# class ChargebackError(Exception):
-#
-#     def __init__(self, message):
-#         self.message = message
+def obj_to_xml(obj):
+    """Convert object to xml string without namespaces
+
+    Args:
+        obj: Object
+
+    Returns:
+        Xml string
+
+    Raises:
+        pyxb.ValidationError
+    """
+    # TODO convert object to xml without default namespace gracefully.
+    try:
+
+        xml = obj.toxml('utf-8')
+    except pyxb.ValidationError as e:
+        raise ChargebackError(e.details())
+    xml = xml.replace(b'ns1:', b'')
+    xml = xml.replace(b':ns1', b'')
+    return xml
+
+
+def generate_retrieval_response(http_response, return_format='dict'):
+    print(http_response.text)
+    return convert_to_format(http_response.text, "LegalEntityRetrievalResponse", return_format)
+
+
+def convert_to_format(http_response, response_type, return_format='dict'):
+    return_format = return_format.lower()
+    if return_format == 'xml':
+        response_xml = http_response.text
+        return response_xml
+    # elif return_format == 'object':
+    #     return convert_to_obj(http_response.text)
+    else:
+        return convert_to_dict(http_response, response_type)
+
+
+# def convert_to_obj(xml_response):
+#     return fields_chargeback.CreateFromDocument(xml_response)
+
+
+def convert_to_dict(xml_response, response_type):
+    response_dict = xmltodict.parse(xml_response)[response_type]
+    if response_dict['@xmlns'] != "":
+        _create_lists(response_dict)
+        return response_dict
+    else:
+        raise ChargebackError("Invalid Format")
+
+
+
+
+def _create_lists(response_dict):
+    if "chargebackCase" in response_dict:
+        _create_list("chargebackCase", response_dict)
+
+        for case in response_dict["chargebackCase"]:
+            if "activity" in case:
+                _create_list("activity", case)
+
+    if "errors" in response_dict:
+        _create_list("error", response_dict["errors"])
+
+
+# if there is only one element for the given key in container, create a list for it
+def _create_list(element_key, container):
+    element_value = container[element_key]
+    if element_value != "" and not isinstance(element_value, list):
+        container[element_key] = [element_value]
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ChargebackError(Exception):
+
+    def __init__(self, message):
+        self.message = message
---
+++ tools/generate.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+import os
+import sys
+
+package_root = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, package_root)
+
+from payfacsdk import utils
+
+version = utils.Configuration().VERSION
+xsd_name = 'merchant-onboard-api-v%s.xsd' % version
+
+# xsd_name = 'merchant-onboard-api-v13.xsd'
+print('Generate module fields using pyxb')
+xsd_abs_path =  os.path.join(package_root, "schema", xsd_name)
+print(xsd_abs_path)
+os.system('pyxbgen -u %s -m fields_payfac' % xsd_abs_path)
+#
+# print('Copy fields_payfac.py to package')
+# gen_field_py_abs_path = os.path.join(package_root, 'tools', 'fields_payfac.py')
+# target_field_py_abs_path = os.path.join(package_root, 'payfacsdk', 'fields_payfac.py')
+# os.system('mv %s %s' % (gen_field_py_abs_path, target_field_py_abs_path))
+#
+
+
